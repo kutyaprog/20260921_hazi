0a40b8f63f73c457d2240940b811a8aed30a68d9
kutya <raduly.tamaas@gmail.com>
2026-09-21 14:09:10 +0200

    Create hazi.py

---
+++ hazi.py
@@ -0,0 +1,77 @@
+#1 feladat
+"""
+egyikSzam = int(input("egyik"))
+masikSzam = int(input("masikSzam"))
+
+b= bool
+b = egyikSzam>0 and masikSzam>0
+print(b)
+
+if egyikSzam <4 and masikSzam != 6:
+    print("igazE")
+
+if egyikSzam or masikSzam == 0:
+    print("Egyik nulla")
+
+if egyikSzam ==5 or masikSzam != 4:
+    print("igazE")
+
+if egyikSzam <=5 or masikSzam >= 13:
+    print("IgazE") 
+
+if egyikSzam >0 and masikSzam <0:
+    print("idk")
+"""
+#2
+
+zold = '\033[91m'
+nz = '\033[0m'
+
+print(f"{nz}\t Negáció\nA\t{zold}NOT A\n{nz}I\t{zold}H\n{nz}H\t{zold}I{nz}")
+print(f"{nz}\t Vagy\nA\tB\t{zold}A or B\n{nz}I\tI\t{zold}I\n{nz}H\tI\t{zold}I\n{nz}I\tH\t{zold}I\n{nz}H\tH\t{zold}H\n{nz}")
+print(f"{nz}\t És\nA\tB\t{zold}A and B\n{nz}I\tI\t{zold}I\n{nz}H\tI\t{zold}H\n{nz}I\tH\t{zold}H\n{nz}H\tH\t{zold}H\n{nz}")
+print(f"{nz}\t Kizáró vagy\nA\tB\t{zold}A and B\n{nz}I\tI\t{zold}H\n{nz}H\tI\t{zold}I\n{nz}I\tH\t{zold}I\n{nz}H\tH\t{zold}H\n{nz}")
+
+#3
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
